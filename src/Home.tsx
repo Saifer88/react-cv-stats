fa9b55285e44613722047ca6bf3f3f8c0a828521
Mario <mariopirris@gmail.com>
2022-04-07 12:14:34 +0200

    icons

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,6 +3,10 @@ import CountUp from "react-countup";
 import { Link } from "react-router-dom";
 import { fetchData } from "./AsyncFunctions";
 import Page from "./Page";
+import { FaAmazon, FaBacterium, FaBed, FaHouseDamage, FaLungsVirus } from 'react-icons/fa';
+import { IconType } from "react-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBedPulse, faCircle, faHeart, faHospital, faHouseMedical, faSkullCrossbones, faThermometer, faVirusCovid, faViruses } from "@fortawesome/free-solid-svg-icons";
 
 interface HomeProps {
 
@@ -14,6 +18,10 @@ interface HomeState {
     intensiveTherapy: String;
     recovered: String;
     home: String;
+    deceduti: String;
+    guariti: String;
+    casi: String;
+    varTotalCases: String;
 }
 
 const nationalUrl = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale-latest.json";
@@ -29,33 +37,50 @@ export default class Home extends React.Component<HomeProps, HomeState>{
             intensiveTherapy: null,
             recovered: null,
             home: null,
+            deceduti: null,
+            guariti: null,
+            casi:null,
+            varTotalCases: null
         };
+    }
+
+    getBacteriumIcon(){
+        return (<FaBacterium></FaBacterium>);
     }
 
     async componentDidMount() { 
         let nationalData = (await fetchData(nationalUrl))[0];
+
+        console.log('data', nationalData);
         
         this.setState({
             'totalCasesToday': nationalData.nuovi_positivi,
             'totalCases': nationalData.totale_positivi,
+            'varTotalCases': nationalData.variazione_totale_positivi,
             'intensiveTherapy': nationalData.terapia_intensiva,
             'recovered': nationalData.ricoverati_con_sintomi,
-            'home': nationalData.isolamento_domiciliare
+            'home': nationalData.isolamento_domiciliare,
+            'deceduti': nationalData.deceduti,
+            'guariti': nationalData.dimessi_guariti,
+            'casi': nationalData.totale_casi
         })
     }
 
     render() {
         return (<div>
+          <div className="row"> 
+          <Card lenght="4" icona={faVirusCovid} title="Casi" value={this.state.casi} diff={null}/>
+          <Card lenght="4" icona={faSkullCrossbones} title="Deceduti" value={this.state.deceduti} diff={null}/>
+            <Card lenght="4" icona={faHeart} title="Guariti" value={this.state.guariti} diff={null}/>
+          </div>
         <div className="row">
-            <Card lenght="6" title="Nuovi positivi:" value={this.state.totalCasesToday}/>
+            <Card lenght="6" icona={faViruses} title="Nuovi positivi" value={this.state.totalCasesToday} diff={null}/>
+            <Card lenght="6" icona={faThermometer} title="Totale positivi" value={this.state.totalCases} diff={this.state.varTotalCases}/>
         </div>
         <div className="row">
-            <Card lenght="12" title="Totale positivi:" value={this.state.totalCases}/>
-        </div>
-        <div className="row">
-            <Card lenght="4" title="Terapia Intensiva:" value={this.state.intensiveTherapy}/>
-            <Card lenght="4" title="Ricoverati con sintomi:" value={this.state.recovered}/>
-            <Card lenght="4" title="Isolamento domiciliare:" value={this.state.home}/>
+            <Card lenght="4" icona={faBedPulse} title="Terapia Intensiva" value={this.state.intensiveTherapy} diff={null}/>
+            <Card lenght="4" icona={faHospital} title="Ricoverati con sintomi" value={this.state.recovered} diff={null}/>
+            <Card lenght="4" icona={faHouseMedical} title="Isolamento domiciliare" value={this.state.home} diff={null}/>
         </div>
     </div>
     )
@@ -64,20 +89,44 @@ export default class Home extends React.Component<HomeProps, HomeState>{
 }
 
 interface CardProps {
-    lenght: String,
-    title: String,
-    value: String
+    lenght: String;
+    title: String;
+    value: String;
+    icona: any;
+    diff: String;
 }
 
 function Card(props: CardProps) {
     return (
-        <div className={"justify-content-end col-"+props.lenght}>
-        <div className="card text-white bg-dark mb-3">
+<div className={"mb-4 col-"+props.lenght}>
+      <div className="card text-white bg-dark mb-3">
         <div className="card-body">
-            <h5 className="card-title">{props.title}</h5>
-            <h1 className="card-text right"><b><CountUp duration={1.5} end={props.value}></CountUp></b></h1>
+          <div className="row">
+            <div className="col-3 d-flex align-items-center">
+            <h1><FontAwesomeIcon mask={faCircle} size="2x" transform="shrink-10" icon={props.icona} /></h1>
+            </div>
+          <div className="col-5  d-flex align-items-center">
+            <h4 className="center">{props.title}</h4>
+          </div>
+          <div className="col-2 d-flex align-items-center">
+            <h4>
+            <CountUp  duration={1.5} separator="." end={props.value}></CountUp>
+            </h4>
+          </div>
+          <div className="col-2 d-flex align-items-center">
+          <h4 className={+props.diff > 0 ? 'red' : 'green'}>
+            {+props.diff > 0  && '+'}{props.diff && (<CountUp  duration={1.5} separator="." end={props.diff}></CountUp>)}
+          </h4>
+          </div>
+
+          </div>
         </div>
-        </div>
-    </div>
+      </div>
+      </div>
+
+
     )
+
+
+
 }
--- src/Page.tsx
+++ src/Page.tsx
@@ -11,6 +11,7 @@ export default class Page extends React.Component{
         return (
     <div className="page">
       <Routes>
+        <Route path="react-cv-stats" element={<Home />}/>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="component" element={<Component />} />
