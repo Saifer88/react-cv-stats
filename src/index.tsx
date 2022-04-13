af8681e514bcd953e46936f71fbfc6c00b444721
Mario <mariopirris@gmail.com>
2022-04-13 20:31:32 +0200

    donate

--- src/index.tsx
+++ src/index.tsx
@@ -8,15 +8,24 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 import 'bootstrap/js/dist/dropdown';
 import Popper from 'popper.js';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
+const initialOptions = {
+  "client-id": "AZi7XZF4W5hPyz0MwJc01B7XVobFjJ9NOyevsRzn1-0wrBEwdhGIdoLCoT6x9auYkwzO0uMYCSlQrsfy",
+  currency: "USD",
+  intent: "capture",
+  "data-client-token": "abc123xyz==",
+};
 
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 root.render(<React.StrictMode>
+  <PayPalScriptProvider options={initialOptions}>
   <BrowserRouter>
   <div className='app'>
   <App></App>
   </div>
   </BrowserRouter>
+  </PayPalScriptProvider>
   </React.StrictMode>);
--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 
-
 export default class Support extends React.Component {
 
     render() {
-        return (<div>Support page</div>)
+        return (<div><p>
+            Se ti piace quello che facciamo, offrici una birra
+            </p></div>)
     }
 }
