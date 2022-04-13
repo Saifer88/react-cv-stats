f968587202a1bce607a0d9505b6d95d7a66f2927
Mario <mariopirris@gmail.com>
2022-04-14 00:16:12 +0200

    arrow

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
 
+import arrow from "../assets/arrow.gif";
+
 export default class Support extends React.Component {
 
     render() {
         return (<div><p>
             Se ti piace quello che facciamo, offrici una birra
-            </p></div>)
+            </p>
+            <img className="support-arrow" width="230" height="230" src={arrow}></img>
+            </div>)
     }
 }
